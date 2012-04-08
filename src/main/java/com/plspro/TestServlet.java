57d6e1054b016fc86feb23dc58f18d4a02f813f4
Viacheslav Krot <vkrot@plslogistics.com>
2012-04-08 23:17:28 +0300

    Added beans.xml to enable CDI and test bean

--- src/main/java/com/plspro/TestServlet.java
+++ src/main/java/com/plspro/TestServlet.java
@@ -1,17 +1,25 @@
 package com.plspro;
 
 import java.io.IOException;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.plspro.service.TestEJB;
+
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+		
+	@Inject
+	private TestEJB bean;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("<h1>It works</h1>");
+		response.getWriter().print(String.format("<h1>%s</h1>", bean.greet()));
 	}
+
 }
