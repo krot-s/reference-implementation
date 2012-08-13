5af7d89988e0ca9f0e2abbc9013558d07fd96064
Viacheslav Krot <vkrot@plslogistics.com>
2012-08-13 18:44:04 +0300

    Changed checkstyle rules

--- reference-implementation/src/main/java/com/plspro/TestServlet.java
+++ reference-implementation/src/main/java/com/plspro/TestServlet.java
@@ -11,15 +11,37 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.plspro.service.TestEJB;
 
+/**
+ * Test servlet.
+ * 
+ * @author User
+ *
+ */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
+	/**
+	 * dummy.
+	 */
 	private static final long serialVersionUID = 1L;
 		
+	/**
+	 * test service.
+	 */
 	@Inject
 	private TestEJB bean;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print(String.format("<h1>Master branch v7: %s</h1>", bean.greet()));		
+	
+	/**
+	 * Test method.
+	 * @param request 
+	 * @param response 
+	 * @throws ServletException 
+	 * @throws IOException 
+	 */
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) 
+	throws ServletException, IOException {
+		response.getWriter().print(
+				String.format("<h1>Master branch v7: %s</h1>", bean.greet()));		
 	}
 
 }
--- reference-implementation/src/main/java/com/plspro/service/TestEJB.java
+++ reference-implementation/src/main/java/com/plspro/service/TestEJB.java
@@ -4,9 +4,20 @@ import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
 
+/**
+ * Test EJB.
+ * 
+ * @author User
+ *
+ */
 @SessionScoped
 public class TestEJB implements Serializable {
-	public String greet(){
+	/**
+	 * Test method.
+	 * 
+	 * @return void
+	 */
+	public String greet() {
 		return "Hello from EJB!";
 	}
 }
